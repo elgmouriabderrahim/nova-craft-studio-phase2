12c852de574881363a942105bd2e6218e74d840f
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-12-18 11:11:24 +0000

    restructure the front controller and configurate htaccess file

--- views/services.php
+++ views/services.php
@@ -1,5 +1,5 @@
 <?php
-require $_SERVER["DOCUMENT_ROOT"]."/data/services.php";?>
+require $_SERVER["DOCUMENT_ROOT"]."/../data/services.php";?>
 <section class="container mx-auto py-16">
     <h2 class="text-3xl font-bold mb-8 text-center"><?php echo count($services) === 0 ? "aucune Service disponible": "Nos Services"?></h2>
     <div class="grid md:grid-cols-3 gap-8">
