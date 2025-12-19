72187342523a12e68731a76ea9aaeb5733f9097d
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-12-19 13:13:06 +0000

    add sign up page validation and storing user data in database

--- app/controllers/404.php
+++ app/controllers/404.php
@@ -4,5 +4,5 @@ http_response_code(404);
 $title = "404 Not Found";
 $view = __DIR__ . '/../views/404.php';
 
-require __DIR__ . '../templates/layout.php';
+require __DIR__ . '/../templates/layout.php';
 exit;
