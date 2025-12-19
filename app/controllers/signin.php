154e32d11c796271ed4e96f715ddc3aec98e0bab
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-12-19 13:36:51 +0000

    add sign in page with authenrication and input validation

--- app/controllers/signin.php
+++ app/controllers/signin.php
@@ -1,5 +1,58 @@
 <?php
-$title = "NovaCraft Studio - Sign In ";
+require_once __DIR__ . '/../../config/database.php';
+
+if (isset($_SESSION['logged'])) {
+    header('Location: /');
+    exit;
+}
+
+$errors = [];
+$email = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    function test($data) {
+        return htmlspecialchars(trim(stripslashes($data)));
+    }
+    $email = test($_POST['email'])?? '';
+    $password = $_POST['password'] ?? '';
+
+    if (empty($email)) {
+        $errors['email'] = 'Email required';
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = 'INVALID EMAIL';
+    }
+
+    if (empty($password)) {
+        $errors['password'] = 'password required';
+    }
+
+    if (empty($errors)) {
+
+        $stmt = $mysqli->prepare("SELECT id, name, password FROM users WHERE email = ?");
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows === 1) {
+            $user = $result->fetch_assoc();
+
+            if (password_verify($password, $user['password'])) {
+                $_SESSION['logged'] = true;
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['user_name'] = $user['name'];
+
+                header('Location: /');
+                exit;
+            } else {
+                $errors['password'] = 'wrong password';
+            }
+        } else {
+            $errors['email'] = "account doesn't exist";
+        }
+    }
+}
+
+$title = "NovaCraft Studio - Sign In";
 $view = __DIR__ . '/../views/signin.php';
 
 require __DIR__ . '/../templates/layout.php';
