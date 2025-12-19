9c443523c3f71367248e2f4228899b08d2976b62
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-12-19 14:34:31 +0000

    fix nav to show and hide if logged in

--- app/templates/header.php
+++ app/templates/header.php
@@ -1,25 +1,30 @@
 <header class="bg-white shadow-md">
   <nav class="container mx-auto flex justify-between items-center py-4">
     <h1 class="text-2xl font-bold text-blue-600">DigitalWave</h1>
-    <?php $current = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); ?>
+    <?php $current = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    if($current === '/signin' ||  $current === '/signup')
+      $show = 'hidden';
+    else
+      $show = 'inline';
+    ?>
     <ul class="flex space-x-6">
       <li>
-        <a href="/" class="<?= $current === '/' ? 'text-blue-600 font-medium' : 'hover:text-blue-600' ?>">Accueil</a>
+        <a href="/" class="<?php echo $current === '/' ? 'text-blue-600 font-medium' : 'hover:text-blue-600'; echo $show?>">Accueil</a>
       </li>
 
       <li>
-        <a href="/services" class="<?= $current === '/services' ? 'text-blue-600 font-medium' : 'hover:text-blue-600' ?>">Services</a>
+        <a href="/services" class="<?php echo $current === '/services' ? 'text-blue-600 font-medium' : 'hover:text-blue-600'; echo $show?>">Services</a>
       </li>
 
       <li>
-        <a href="/about" class="<?= $current === '/about' ? 'text-blue-600 font-medium' : 'hover:text-blue-600' ?>">A propos</a>
+        <a href="/about" class="<?php echo $current === '/about' ? 'text-blue-600 font-medium' : 'hover:text-blue-600'; echo $show ?>">About us</a>
       </li>
 
       <li>
-        <a href="/contact" class="<?= $current === '/contact' ? 'text-blue-600 font-medium' : 'hover:text-blue-600' ?>">Contact</a>
+        <a href="/contact" class="<?php echo $current === '/contact' ? 'text-blue-600 font-medium' : 'hover:text-blue-600'; echo $show?>">Contact Us</a>
       </li>
       <li>
-        <a class="px-8  border border-gray-600 rounded-md hover:bg-neutral-100 <?= ($current === '/signin' ||  $current === '/signup') ? 'hidden' : 'inline' ?>" href = "/logout">Sign Out</a>
+        <a class="px-8  border border-gray-600 rounded-md hover:bg-neutral-100 <?php echo $show ?>" href = "/logout">Sign Out</a>
       </li>
     </ul>
   </nav>
