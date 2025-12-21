4102471bf4eed35e10ac34ea62ca32fa611cd966
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-12-21 12:56:26 +0000

    fix contact view tomatch the logic and display errors

--- app/views/contact.php
+++ app/views/contact.php
@@ -1,42 +1,39 @@
 <section class="container mx-auto py-16">
-    <?php
-    if (preg_match($name_regex, $name) && filter_var($email, FILTER_VALIDATE_EMAIL) && strlen($message) >= 5):
-        echo "<div class='max-w-xl mx-auto bg-green-100 text-green-700 p-4 rounded mb-4'>
-        Merci $name! Votre message a ete envoye avec succes.
-        </div>";
-        $name = $email = $message = "";
-        $succes = true;
-    endif;
-    ?>
-    
-    <h2 class="text-3xl font-bold mb-6 text-center">Contactez-nous</h2>
-    <form class="max-w-xl mx-auto bg-white p-8 shadow-md rounded-lg space-y-4" action="https://formspree.io/f/xqarlyaz" method="POST">
-        <input type="text" name="name" placeholder="Votre nom" class="w-full border px-4 py-2 rounded-lg" value="<?php echo $name; ?>">
-        <span class= 'text-red-500'>*</span>
-        <?php if(!empty($_POST) && !preg_match($name_regex, $name) && !$succes)
-            if($name == "")
-                echo "<span class= 'text-red-500'>please eneter your name</span>";
-            else
-                echo "<span class= 'text-red-500'>please eneter a valid name</span>";
-        ?>
 
-        <input type="email" name="email" placeholder="Votre email" class="w-full border px-4 py-2 rounded-lg" value="<?php echo $email; ?>">
-        <span class= 'text-red-500'>*</span>
-        <?php if(!empty($_POST) && !filter_var($email, FILTER_VALIDATE_EMAIL) && !$succes)
-            if($email == "")
-                echo "<span class= 'text-red-500'>please eneter your email</span>";
-            else
-                echo "<span class= 'text-red-500'>please eneter a valid email</span>";
-        ?>
+    <?php if ($success): ?>
+    <div class="max-w-xl mx-auto bg-green-100 text-green-700 p-4 rounded mb-4">
+        Merci <?=$name ?> ! Votre message a été envoyé avec succès.
+    </div>
+    <?php endif; ?>
 
-        <textarea placeholder="Votre message" name="message" class="w-full border px-4 py-2 rounded-lg"><?php echo $message; ?></textarea>
-        <span class= 'text-red-500'>*</span>
-        <?php if(!empty($_POST) && strlen($message) < 5 && !$succes)
-            if($message == "")
-                echo "<span class= 'text-red-500'>please eneter a message</span>";
-            else
-                echo "<span class= 'text-red-500'>message must have at least 5 characters</span>";
-        ?>
-        <button class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700">Envoyer</button>
+    <h2 class="text-3xl font-bold mb-6 text-center">Contact us</h2>
+
+    <form class="max-w-xl mx-auto bg-white p-8 shadow-md rounded-lg space-y-4" method="POST">
+
+        <input type="text" name="name" placeholder="your name" class="w-full border px-4 py-2 rounded-lg" value="<?= $name ?>">
+
+        <?php if (isset($errors['name'])): ?>
+        <span class="text-red-500"><?= $errors['name'] ?></span>
+        <?php endif; ?>
+
+        <input type="email" name="email" placeholder="Votre email" class="w-full border px-4 py-2 rounded-lg" value="<?= $email ?>">
+
+        <?php if (!empty($errors['email'])): ?>
+        <span class="text-red-500"><?= $errors['email'] ?></span>
+        <?php endif; ?>
+
+        <textarea name="message" placeholder="Votre message" class="w-full border px-4 py-2 rounded-lg"><?= $message ?></textarea>
+
+        <?php if (!empty($errors['message'])): ?>
+        <span class="text-red-500"><?= $errors['message'] ?></span>
+        <?php endif; ?>
+
+        <?php if (!empty($errors['general'])): ?>
+        <span class="text-red-500"><?= $errors['general'] ?></span>
+        <?php endif; ?>
+
+        <button class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700">
+            Envoyer
+        </button>
     </form>
 </section>
